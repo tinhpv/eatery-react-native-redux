0643ad8a08074a436b7447aabafdad9ed5cbff0b
tinhpv <vutinh.phan@gmail.com>
2022-01-06 01:48:55 +0700

    🍕🔧 fix VirtualizedList warning

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,28 +1,41 @@
 import React, {useState} from 'react';
-import {View, ScrollView, SafeAreaView, StyleSheet, LogBox} from 'react-native';
+import {
+  View,
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
 
 import HomeHeader from './components/HomeHeader';
 import EateryList from './components/EateryList';
-
-LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
 
 const HomeScreen = ({navigation}) => {
   const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View key={1} style={styles.headerSection}>
-          <HomeHeader
-            onSearchSubmit={term => {
-              setSearchTerm(term);
-            }}
-          />
-        </View>
-        <View key={2}>
-          <EateryList searchTerm={searchTerm} navigateHandler={navigation} />
-        </View>
-      </ScrollView>
+      <FlatList
+        data={[]}
+        renderItem={() => null}
+        ListFooterComponent={
+          <>
+            <View style={styles.headerSection}>
+              <HomeHeader
+                onSearchSubmit={term => {
+                  setSearchTerm(term);
+                }}
+              />
+            </View>
+            <View>
+              <EateryList
+                searchTerm={searchTerm}
+                navigateHandler={navigation}
+              />
+            </View>
+          </>
+        }
+      />
     </SafeAreaView>
   );
 };
