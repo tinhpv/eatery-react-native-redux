a357ad47137a9a5948f6ea62a314a10f9b29cbe8
tinhpv <vutinh.phan@gmail.com>
2022-01-06 01:50:03 +0700

    🍕❌ remove unused component import

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, TextInput, Text} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const SearchBar = ({placeholder, searchTerm, setSearchTerm, onSubmit}) => {
